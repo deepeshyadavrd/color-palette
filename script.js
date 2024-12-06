e5ad9359601564e870e76604af71b9447ee76576
deepeshyadavrd <raoshsbh6@gmail.com>
2024-12-06 17:06:08 +0530

    function for generating new analogous color palette

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const searchInput = document.querySelector(".search-input"),
     randomBtn = document.querySelector("#random-btn"),
     paletteContainer = document.querySelector("#palette"),
     relatedContainer = document.querySelector("#related");
+
+    let currentColor = "skyblue",
+        currentType = "analogous",
+        currentCount = 6,
+        imageColors = [];
+
+
+// all functions to generate different color palette
+
+function generateAnalogousPalette(hsl, count) {
+    const palette = [];
+    const [hue, saturation, lightness] = hsl;
+
+    for(let i=0; i < count; i++){
+        let newHue = hue + 30 * i;
+        if(newHue > 360){
+            newHue -=360;
+        }
+
+        palette.push([newHue, saturation, lightness])
+    }
+}
