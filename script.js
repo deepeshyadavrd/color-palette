2ebfbdedf001659e7622e11903d81312865c77ab
deepeshyadavrd <raoshsbh6@gmail.com>
2024-12-11 17:50:20 +0530

    function to convert hsl to hex color code

--- script.js
+++ script.js
@@ -250,8 +250,14 @@ function HslToHex(hsl){
     let l = hsl[2];
 
         l /= 100;
-        const a =(s * Math.min())
+        const a =(s * Math.min(l, 1-l)) / 100;
+        const f = (n) => {
+            const k =(n+h/30) % 12;
+            const color = l-a * Math.max(Math.min(k - 3, 9-k,1), -1);
+            return Math.round(255 * color).toString(16).padStart(2,"0")
+        }
+        return `#${f(0)}${f(8)}${f(4)}`;
 }
-let rgb = [0,255,255];
+let hsl = [100,50,50];
 
-console.log(rgbToHsl(rgb));
+console.log(HslToHex(hsl));
