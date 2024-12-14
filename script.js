feba426edc53fa03cba045b4c2b448df2031bcb2
deepeshyadavrd <raoshsbh6@gmail.com>
2024-12-14 17:22:38 +0530

    generate random color

--- script.js
+++ script.js
@@ -293,3 +293,18 @@ countSelect.addEventListener("change", (e) => {
     currentCount = value;
     generatePaletteHtml(currentType, paletteContainer);
 })
+
+randomBtn.addEventListener("click", () => {
+    const randomColor = getRandomColor();
+})
+
+function getRandomColor(){
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for(let i =0; i<6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+console.log(getRandomColor());
